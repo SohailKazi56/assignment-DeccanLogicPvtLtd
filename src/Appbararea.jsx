b98f93ab8b950c18927732e3e680e66599520c9a
SohailKazi56 <sohailkazi56@gmail.com>
2020-11-01 16:07:53 +0530

    first commit towards assignment

---
+++ src/Appbararea.jsx
@@ -0,0 +1,127 @@
+import { fade, makeStyles } from '@material-ui/core/styles';
+import {React} from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import Badge from '@material-ui/core/Badge';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import { People, Message } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  searchIcon: {
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+}));
+
+export default function PrimarySearchAppBar() {
+   const classes = useStyles();
+
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static" style={{backgroundColor:"#ffc324"}}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            Advancing For Humanity
+          </Typography>
+          <div className={classes.search} style={{width:"30%",borderRadius:"50px",backgroundColor:"#004d9a",position:"relative",margin:"0px 20%"}}>
+            <div className={classes.searchIcon} >
+              <SearchIcon style={{padding:"8px"}}/>
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            <IconButton aria-label="show 1 new request" color="inherit">
+              <Badge badgeContent={1} color="secondary">
+                <People />
+              </Badge>
+            </IconButton>
+            <IconButton aria-label="show 17 new notifications" color="inherit">
+              <Badge badgeContent={"99+"} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              edge="end"
+              aria-label="message-box"
+              color="inherit"
+            >
+              <Message />
+            </IconButton>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-haspopup="true"
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
---
+++ src/Form.jsx
@@ -0,0 +1,350 @@
+import React from 'react';
+import './App.css';
+import { Select,MenuItem, FormGroup, InputAdornment, Input, IconButton, InputLabel } from '@material-ui/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faImage } from '@fortawesome/free-regular-svg-icons';
+import { faBold, faClock, faItalic, faPaperclip, faListOl, faList, faPhotoVideo, faReply, faShare, faAlignCenter } from '@fortawesome/free-solid-svg-icons';
+import { Grid, Cell, Switch} from 'react-mdl';
+
+ const Form = () => {
+
+    // event getting value
+    const [eventType, setEventType] = React.useState('');
+    const handleEventChange = (event) => {
+        setEventType(event.target.value);
+    };
+
+    // category getting value
+    const [categoryType, setCategoryType] = React.useState('');
+    const handleCategoryChange = (event) => {
+        setCategoryType(event.target.value);
+    };
+
+    // timezone getting value
+    const [TimezoneType, setTimezoneType] = React.useState('');
+    const handleTimezoneChange = (event) => {
+        setTimezoneType(event.target.value);
+    };
+
+    // Short summary getting value
+    const [summary, setSummary] = React.useState('');
+    const handleSummary = (event) => {
+        setSummary(event.target.value);
+    }
+
+    // start date getting value
+    const [sdate, setSdate ] = React.useState('');
+    const handleChangeSdate = (event) => {
+        setSdate(event.target.value);
+    }
+
+    // end date getting value
+    const [edate, setEdate ] = React.useState('');
+    const handleChangeEdate = (event) => {
+        setEdate(event.target.value);
+    }
+
+    // start time getting value
+    const [stime, setStime ] = React.useState('');
+    const handleChangeStime = (event) => {
+        setStime(event.target.value);
+    }
+
+    // end time getting value
+    const [etime, setEtime ] = React.useState('');
+    const handleChangeEtime = (event) => {
+        setEtime(event.target.value);
+    }
+
+    const [textValue, setTextValue ] = React.useState('');
+    const handleTextValue = (event) => {
+        setTextValue(event.target.value);
+    }
+
+    // online link getting value
+    const [onlineLink, setOnlineLink ] = React.useState('');
+    const handleOlLink = (event) => {
+        setOnlineLink(event.target.value);
+    }
+
+    // type public/private  getting value
+    const [typeValue, setTypeValue ] = React.useState('');
+    const handleType = (event) => {
+        setTypeValue(event.target.value);
+    }
+
+    // title getting value
+    const [titleValue, setTitleValue ] = React.useState('');
+    const handleTitle = (event) => {
+        setTitleValue(event.target.value);
+    }
+
+    // description box getting value
+    const [descriptionType, setDescriptionType] = React.useState('');
+    const handleDescriptionChange = (event) => {
+        setDescriptionType(event.target.value);
+    }
+
+    //submit button function
+    const submittion = () => {
+        alert(`Account for ${titleValue} created successfully`);
+    }
+
+    return (
+        <div>
+
+            {/* create cancel button code */}
+            <Grid>
+                <Cell col={8}>
+                    <h6 style={{fontSize:"18px", fontWeight:"bold", color:"blue",margin:"0px"}}>Create Event</h6>
+                </Cell>
+                <Cell col={4} >
+                    <button  className="cancel-button" style={{float:"right"}}>Cancel</button>
+                    <button  className="create-button" style={{float:"right"}} type="submit" onClick={submittion} >Create</button>
+                </Cell>
+            </Grid>
+                
+            <hr style={{marginTop:"-5px"}}/>
+
+            {/*  form code */}
+            <div className="container-form">
+                <form  className="form" style={{width:"50%", margin:"auto"}}>
+
+                {/* add event type code */}
+                <FormGroup>
+                <label >Add Event in *</label>
+                <Select
+                    style={{width:"100%"}}
+                    labelId="select-label"
+                    placeholder="Select..."
+                    id="event-id"
+                    value={eventType}
+                    onChange={handleEventChange}>
+                
+                <MenuItem value={"events type 1"} >Event 1</MenuItem>
+                <MenuItem value={"events type 2"} >Event 2</MenuItem>
+                <MenuItem value={"events type 3"} >Event 3</MenuItem>
+
+                </Select>
+                </FormGroup>
+                
+                {/* upload image code */}
+                <button style={{padding:"18px",height:'96px',width:"96px",backgroundColor:"navy",textAlign:"center",margin:"16px 0px"}}>
+                    <h6 style={{backgroundColor:"gray", color:"white", margin:"auto",borderRadius:"5px",fontSize:"16px"}}>
+                    Upload image
+                    </h6>
+                </button>
+                
+                {/* title ui code */}
+                <FormGroup>
+                    <label >Title *</label>
+                    <Input 
+                    placeholder=""
+                    onChange={handleTitle}
+                    value={titleValue}
+                    style={{width:"100%"}}
+                    />
+                    <label style={{textAlign:"right",color:"gray",fontSize:"12px"}}>(0/300)</label>
+                </FormGroup>
+                
+                {/* categories ui code */}
+                <FormGroup>
+                    <label >Categories *</label>
+                    <Select
+                        style={{width:"100%"}}
+                        labelId="category-select"              
+                        id="category-id"
+                        value={categoryType}
+                        onChange={handleCategoryChange}>
+                    
+                    <MenuItem value={"category type 1"} >category 1</MenuItem>
+                    <MenuItem value={"category type 2"} >category 2</MenuItem>
+                    <MenuItem value={"category type 3"} >category 3</MenuItem>
+
+                    </Select>
+                </FormGroup>
+
+                {/* short summary ui code */}
+                <FormGroup style={{marginTop:"12px"}}>
+                    <label >Short Summary *</label>
+                    <Input 
+                        onChange={handleSummary}
+                        value={summary}
+                        placeholder=""
+                        style={{width:"100%"}}
+                    />
+                    <label style={{marginTop:"0px",textAlign:"right",color:"gray",fontSize:"12px"}}>(0/300)</label>
+                </FormGroup>
+
+                {/* registration site ui code */}
+                <FormGroup style={{marginTop:"0px"}}>
+                    <label style={{fontWeight:"bold"}}>Type:  Public *</label>
+                    <Input 
+                    onChange={handleType}
+                    value={typeValue}
+                    placeholder="Registration site"
+                    style={{width: '100%',marginTop:"0px"}}
+                    />
+                    <label style={{textAlign:"right",color:"gray",fontSize:"12px"}}>(0/300)</label>
+                </FormGroup>    
+
+                {/* virtual event online link ui code */}
+                <Grid style={{margin:"0px",marginLeft:"-15px"}}>
+                    <Cell col={3}><label>Is this a virtual event ?</label></Cell>
+                    <Cell col={1}><span > Yes </span></Cell>
+                    <Cell col={1}><Switch defaultChecked /></Cell>
+                </Grid>
+
+                <Input 
+                onChange={handleOlLink}
+                value={onlineLink}
+                placeholder="online link"
+                style={{width: '100%',marginTop:"-35px"}}
+                /> 
+                
+                {/* timezone ui code */}
+                <FormGroup style={{marginTop:"12px"}}>
+                    <label>Select Timezone *</label>
+                    <FormGroup style={{display:"block"}}>
+                    <Select
+                    style={{width:"70%"}}
+                    labelId="timezone-select"
+                    placeholder="Select Time..."
+                    id="timezone-id"
+                    value={TimezoneType}
+                    onChange={handleTimezoneChange}>
+                
+                    <MenuItem value={"city-1"} >Pune</MenuItem>
+                    <MenuItem value={"city-2"} >Mumbai</MenuItem>
+                    <MenuItem value={"city-3"} >Bangalore</MenuItem>
+                    <MenuItem value={"city-4"} >Delhi</MenuItem>
+
+                    </Select>
+                    <span style={{fontWeight:"bold",marginLeft:"8px"}}>GMT +5:30 ?</span>
+                    </FormGroup>
+                </FormGroup>
+
+                <Grid style={{width:"70%",margin:"0px", marginLeft:"-15px"}}>
+                    <Cell col={6}>
+                        <FormGroup >
+                            <label >Start Date *</label>
+                            <Input 
+                            id="start-date-id"
+                            value={sdate}
+                            onChange={handleChangeSdate}
+                            />
+                        </FormGroup>
+                    </Cell>
+                    <Cell col={6}>
+                        <FormGroup >
+                            <label >Start Time *</label>
+                            <Input
+                            id="start-time-id"
+                            value={stime}
+                            onChange={handleChangeStime}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                <FontAwesomeIcon icon={faClock}  />
+                                </InputAdornment>
+                            }
+                            />
+                        </FormGroup>
+                    </Cell>
+                </Grid>
+
+                <Grid style={{width:"70%",margin:"0px", marginLeft:"-15px"}} >
+                    <Cell col={6}>
+                        <FormGroup >
+                            <label >End Date *</label>
+                            <Input 
+                            id="end-date-id"
+                            value={edate}
+                            onChange={handleChangeEdate}
+                            />
+                            
+                        </FormGroup>
+                    </Cell>
+                    <Cell col={6}>
+                        <FormGroup >
+                            <label >End Time *</label>
+                            <Input
+                            id="end-time-id"
+                            value={etime}
+                            onChange={handleChangeEtime}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                <FontAwesomeIcon icon={faClock}  />
+                                </InputAdornment>
+                            }
+                            />
+                        </FormGroup>
+                    </Cell>
+                </Grid>
+                
+                {/* description box ui code */}
+                <div style={{width:"101%",backgroundColor:"whitesmoke",marginBottom:"0px",marginTop:'8px',height:"1%"}}>
+
+                    <Grid style={{height:"20px"}}>
+                        <Cell col={3} style={{margin:"-15px 0px"}}>
+                            <Select
+                                style={{width:"100%",padding:"4px 12px"}}
+                                // defaultValue={""}
+                                id="description-id"
+                                value={descriptionType}
+                                onChange={handleDescriptionChange}>      
+                            <MenuItem value={"para"}>Paragraph</MenuItem>
+                            <MenuItem value={"chapter"} >Chapter</MenuItem>
+                            </Select>
+                            {/* <View style={{height:"100%", width:"2px",}} /> */}
+                        </Cell> 
+                        
+                        <Cell col={9}>
+                        <Grid style={{margin:"-35px 0px"}}>
+                            <Cell col={1}> <IconButton style={{}}><FontAwesomeIcon icon={faImage} /></IconButton> </Cell>
+                            <Cell col={1}> <IconButton> <FontAwesomeIcon icon={faBold} /> </IconButton>  </Cell>
+                            <Cell col={1}> <IconButton> <FontAwesomeIcon icon={faItalic} /></IconButton>  </Cell>
+                            <Cell col={1}> <IconButton> <FontAwesomeIcon icon={faPaperclip} /></IconButton>  </Cell>
+                            <Cell col={1}> <IconButton> <FontAwesomeIcon icon={faList} /> </IconButton>  </Cell>
+                            <Cell col={1}> <IconButton> <FontAwesomeIcon icon={faListOl} /> </IconButton>  </Cell>
+                            <Cell col={1}> <IconButton> <FontAwesomeIcon icon={faImage} /> </IconButton>  </Cell>
+                            <Cell col={1}> <IconButton> <FontAwesomeIcon icon={faPhotoVideo} /> </IconButton>  </Cell>
+                            <Cell col={1}> <IconButton> <FontAwesomeIcon icon={faReply} /> </IconButton>  </Cell>
+                            <Cell col={1}> <IconButton> <FontAwesomeIcon icon={faShare} /> </IconButton>  </Cell>
+                            <Cell col={1}> <IconButton> <FontAwesomeIcon icon={faAlignCenter} /> </IconButton>  </Cell>
+                            <Cell col={1}></Cell>
+                        </Grid>
+                        </Cell>
+                    </Grid>
+                        
+                </div>
+
+                <textarea style={{width:"100%",height:"200px"}}
+                    placeholder="Description (max limit 8000 characters)"
+                    maxLength= "8000"
+                    value={textValue}
+                    onChange={handleTextValue}
+                >
+                </textarea>
+                
+                {/* attachments ui code */}
+                <FormGroup style={{margin:"12px 0px"}}>
+                <label>Attachments ?</label> 
+                <button className="upload-file-btn">
+                    <FontAwesomeIcon icon={faImage} style={{width:"40",height:"40", color:"gray"}} />
+                    <p style={{fontSize:"16px",fontWeight:"bold"}}>Click here to upload file(s)</p>
+                </button>
+                </FormGroup>
+
+                 {/* footer ui code */}               
+                <p style={{marginTop:"48px",textAlign:"center",color:"gray"}}>Use options on the top right section of the screen to save your changes</p>
+                
+
+
+                </form>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Form;
